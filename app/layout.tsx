027c56d977201071211af8009435410d73ed963e
Aden Lofthouse <aden.lofthouse@gmail.com>
2023-04-21 15:26:10 +1000

    made all pages and basic containers

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           {children}
         </div>
       </div>
-      <body className="flex"></body>
+      <body></body>
     </html>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,38 +1,40 @@
+import Link from "next/link";
+
 const Sidebar = () => {
   return (
     <div className="flex h-screen">
-      <div className="bg-sky-950 text-gray-100 w-64 rounded-3xl m-3 text-center">
+      <div className="bg-sky-950 text-gray-100 flex-col w-64 rounded-3xl m-3 text-center">
         <div className="px-4 py-6">
           <h2 className="text-lg font-medium">WMS 2</h2>
         </div>
         <div className="px-4 py-2">
           <a
-            href="#"
-            className="block text-gray-400 hover:bg-white rounded-3xl p-3"
+            href="/dashboard"
+            className="block text-white hover:bg-white hover:text-black focus:bg-emerald-700 rounded-3xl p-3"
           >
             Dashboard
           </a>
           <a
             href="/sales"
-            className="block text-gray-400 hover:bg-white rounded-3xl p-3"
+            className="block text-white hover:bg-white hover:text-black focus:bg-emerald-700 rounded-3xl p-3"
           >
             Sales
           </a>
           <a
-            href="#"
-            className="block text-gray-400 hover:bg-white rounded-3xl p-3"
+            href="/products"
+            className="block text-white hover:bg-white hover:text-black focus:bg-emerald-700  rounded-3xl p-3"
           >
             Products
           </a>
           <a
-            href="#"
-            className="block text-gray-400 hover:bg-white rounded-3xl p-3"
+            href="/purchaseorders"
+            className="block text-white hover:bg-white hover:text-black focus:bg-emerald-700  rounded-3xl p-3"
           >
             Purchase Orders
           </a>
           <a
-            href="#"
-            className="block text-gray-400 hover:bg-white rounded-3xl p-3"
+            href="/contacts"
+            className="block text-white hover:bg-white hover:text-black focus:bg-emerald-700 rounded-3xl p-3"
           >
             Contacts
           </a>
