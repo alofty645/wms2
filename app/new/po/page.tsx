870ce22c7eb25ae878043a965024e2118264c89b
Aden Lofthouse <aden.lofthouse@gmail.com>
2023-04-23 19:49:56 +1000

    new headers

--- app/new/po/page.tsx
+++ app/new/po/page.tsx
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/Header";
 
 export default function Home() {
-  return;
+  return (
+    <div className="m-4">
+      <Header title={"New Purchase Order"}></Header>{" "}
+    </div>
+  );
 }
